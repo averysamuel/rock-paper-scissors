14b5da9c7b2d7e875731e378231e33dc4cb8d783
Avery <avsam21@gmail.com>
2022-02-02 16:19:40 -0500

    added ui to play game from browser

--- javascript.js
+++ javascript.js
@@ -1,54 +1,96 @@
+//initialize variables
+let playerScore = 0;
+let computerScore = 0;
+let gameCounter = 0;
+
 function computerPlay(){
     let computerMove = Math.floor(Math.random()*3);
     if(computerMove === 0) {
-        return "Rock";
+        return "rock";
     } else if (computerMove === 1){
-        return "Paper";
+        return "paper";
     }
-    return "Scissors";
+    return "scissors";
 
 }
-
-function playRound(playSelection,computerSelection){
-    switch(playSelection){
-        
-        case "rock":
-            if(computerSelection == "Paper"){
-                console.log("You Lose! Paper beats Rock")
-                return -1;
-        
-            }else if(computerSelection == "Scissors"){
-                console.log( "You Win! Rock beats Scissors")
-                return 1;
-            }else {
-                console.log("It's a tie")
-                return 0;
-            }
-        case "scissors":
-            if(computerSelection == "Paper"){
-                console.log( "You Win! Scissors beats Rock")
-                return 1
-            }else if(computerSelection == "Rock"){
-                console.log("You Lose! Rock beats Scissors")
-                return -1
-            }else{
-                console.log("It's a tie")
-                return 0;
-            }
-        
-        case "paper":
-            if(computerSelection == "Rock"){
-                console.log("You Win! Paper beats Rock")
-                return 1
-            }else if(computerSelection == "Scissors"){
-                console.log("You Lose! Scissors beats Paper")
-                return -1
-            }else{
-                console.log("It's a tie")
-                return 0;
-            }
-        }
+function playRound(playerSelection,computerSelection){
+    if (playerSelection === computerSelection){
+        return "Tie Game!"
     }
+    if  (playerSelection === 'rock'&& computerSelection === 'paper' ||
+         playerSelection ==='paper' && computerSelection === 'scissors' ||
+         playerSelection === 'scissors' && computerSelection === 'rock') {
+            computerScore++;
+            return `You've lost the round, ${computerSelection} beats ${playerSelection}`;
+ 
+    }
+
+    if ((computerSelection === 'rock'&& playerSelection === 'paper')||
+        (computerSelection ==='paper' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'rock')) {
+            playerScore++;
+            return `You've won the round! ${playerSelection} beats ${computerSelection}`;;
+    }
+}
+
+//initialize HTML variables
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+const results = document.createElement('div');
+const winner = document.createElement('h2');
+body.appendChild(results);
+buttons.forEach(button => button.addEventListener('click',runGame));
+
+function runGame(){
+    if(document.contains(winner)){
+        body.removeChild(winner);
+    }
+    const playerSelection = this.className;
+    const computerSelection = computerPlay();
+    results.textContent = playRound(playerSelection,computerSelection ); 
+    gameCounter++;
+    document.getElementById("player-score").textContent = playerScore;
+    document.getElementById("computer-score").textContent = computerScore;
+    document.getElementById("game-counter").textContent = gameCounter; 
+    document.getElementById("computer-selection").textContent = computerSelection; 
+    
+    if(gameCounter===5){
+    document.getElementById("player-score").textContent = playerScore;
+    document.getElementById("computer-score").textContent = computerScore;
+    document.getElementById("game-counter").itextContent = gameCounter;  
+        endGame()};
+}
+
+
+function endGame(){
+    body.appendChild(winner);
+    if(computerScore === playerScore){
+        winner.textContent = ('T\'was a tie - have another go at it')
+    }else if(computerScore < playerScore){
+        winner.textContent = ('Congratulations, you WON! ...this time...')
+    }else {winner.textContent = ('Seems like you\'ve been defeated, try again!')}  
+    gameCounter = 0;
+    playerScore = 0;
+    computerScore = 0;
+}
+
+
+
+
+
+
+/*
+buttons.forEach((button)=>{
+     button.addEventListener('click',()=>{
+       const playerSelection = button.className;
+       const results = document.createElement('div');
+       results.textContent = playRound(playerSelection,computerPlay()); 
+       body.appendChild(results);
+    });
+});
+*/
+
+  /*
     function game(){
         let counter =0;
         let userInput;
@@ -58,3 +100,4 @@ function playRound(playSelection,computerSelection){
         }
         return (counter<3)?"You Lose":"You Win!";
     }
+    */
